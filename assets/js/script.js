7b912db668014b10dd2f7d3c30711ef063342b60
Julien Sebag <julien.sebag@wibas-etarate.com>
2019-10-06 10:21:36 +0200

    Fixed margins icons bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,51 +28,74 @@ function search() {
      }
 }
 
-//Filters
-filterSelection("all")
+// Filters
+
+filterSelection("all");
+var count = 0;
 function filterSelection(c) {
-     var x, i;
-     x = document.getElementsByClassName("filterDiv");
-     if (c == "all") c = "";
-     // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-     for (i = 0; i < x.length; i++) {
-          w3RemoveClass(x[i], "show");
-          if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-     }
+	clean();
+	var x, i;
+	x = document.getElementsByClassName("filterDiv");
+	if (c == "all") c = "";
+	for (i = 0; i < x.length; i++) {
+		w3RemoveClass(x[i], "show");
+		if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+	}
 }
-
-// Show filtered elements
+function clean() {
+	count = 0;
+	document.querySelectorAll(".filterDiv").forEach(it => {
+		it.classList.forEach(className => {
+			className.startsWith("item") && it.classList.remove(className);
+		});
+	});
+}
 function w3AddClass(element, name) {
-     var i, arr1, arr2;
-     arr1 = element.className.split(" ");
-     arr2 = name.split(" ");
-     for (i = 0; i < arr2.length; i++) {
-          if (arr1.indexOf(arr2[i]) == -1) {
-               element.className += " " + arr2[i];
-          }
-     }
+	var i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+
+	for (i = 0; i < arr2.length; i++) {
+		if (arr1.indexOf(arr2[i]) == -1) {
+			element.className += " " + arr2[i];
+			element.classList.add(getNext());
+		}
+	}
 }
 
-// Hide elements that are not selected
+function getNext() {
+	if (!count) {
+		count = 0;
+	}
+
+	if (count >= 4) {
+		count = 1;
+	} else {
+		count += 1;
+	}
+
+	return "item" + count;
+}
+
 function w3RemoveClass(element, name) {
-     var i, arr1, arr2;
-     arr1 = element.className.split(" ");
-     arr2 = name.split(" ");
-     for (i = 0; i < arr2.length; i++) {
-          while (arr1.indexOf(arr2[i]) > -1) {
-               arr1.splice(arr1.indexOf(arr2[i]), 1);
-          }
-     }
-     element.className = arr1.join(" ");
+	var i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+	for (i = 0; i < arr2.length; i++) {
+		while (arr1.indexOf(arr2[i]) > -1) {
+			arr1.splice(arr1.indexOf(arr2[i]), 1);
+		}
+	}
+	element.className = arr1.join(" ");
 }
 
 // Add active class to the current control button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var btns = btnContainer.getElementsByClassName("tab-select");
 for (var i = 0; i < btns.length; i++) {
-     btns[i].addEventListener("click", function () {
-          var current = document.getElementsByClassName("active");
-          current[0].className = current[0].className.replace(" active", "");
-          this.className += " active";
-     });
+	btns[i].addEventListener("click", function() {
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
+		this.className += " active";
+	});
 }
